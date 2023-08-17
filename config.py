07e062ee8bad174af388782dc10d6094f9aec518
perep <perep111@mail.ru>
2023-08-17 19:52:00 +1000

    Добавил ключи и пароли в переменные окружения и поместил в .gitignore

--- config.py
+++ config.py
@@ -2,10 +2,14 @@ import logging
 from aiogram import Dispatcher, Bot, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 storage = MemoryStorage()
 
-BOT_TOKEN = '6146009037:AAGqU1swh9utMUrOaTVvLlfWb3_1d6Ypb84'
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
 logging.basicConfig(level=logging.INFO)
-openai.api_key = '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'
+openai.api_key = os.getenv('OPENAI_API_KEY')
